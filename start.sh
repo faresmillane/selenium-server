4ffa7efd84538693dbc0d913402664e539653227
Millane <ts-fares.millane@rakuten.com>
2022-03-25 17:10:10 +0100

    delete windows files

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "check availability to start selenium ..."
-for i in 4445 4446 4447; do
+for i in 4444 4445 4446; do
     echo "check port: $i availability..."
     PID=$(lsof -i :$i | tail -n +2 | awk '{print $2}')
     if [ -n "$PID" ]; then
@@ -9,7 +9,8 @@ for i in 4445 4446 4447; do
         echo "the port: $PID is killed..."
     fi
 done
-java -jar ./selenium-server-4.1.2.jar hub --port 4445 &
+chmod 777 drivers/chromedriver drivers/geckodriver
+java -jar ./selenium-server-4.1.2.jar hub --port 4444 &
 sleep 3
 java -jar ./selenium-server-4.1.2.jar node --config ./config/chrome-node.toml &
 sleep 3
