bc51c53e8ee93e24273f87347ec1259817e9eb34
Millane <ts-fares.millane@rakuten.com>
2022-04-01 17:10:31 +0200

    update script: add colors

--- start.sh
+++ start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+yellow=`tput setaf 3`
+white=`tput setaf 7`
 
-echo "check availability to start selenium ..."
+echo "${white}check availability to start selenium ..."
 for i in 4444 4445 4446; do
     PID=$(netstat -ano | findStr "$i" | awk '{print $5}' | head -n 1)
     if [ -n "$PID" ]; then
-        echo "the port: $i is used, tap this command in your powershell terminal to kill it"
-        echo "taskkill /F /PID $PID"
+        echo "${white}the port: $i is used, tap this command in your powershell terminal to kill it :"
+        echo "--> ${yellow}taskkill /F /PID $PID"
     fi
 done
 
 if [ "$PID" ]; then
-    echo "you will kill all this process before starting selelnium server"
-    echo "restart start.sh script after finishing..."
+    echo "${white}you will kill all this process before starting selelnium server"
+    echo "${white}restart start.sh script after finishing..."
 else
     java -jar ./selenium-server-4.1.2.jar hub --port 4444 &
     sleep 3
@@ -19,7 +21,7 @@ else
     sleep 3
     java -jar ./selenium-server-4.1.2.jar node --config ./config/firefox-node.toml &
     sleep 3
-    echo "selenium started : Done"
+    echo "${white}selenium started : Done"
 fi
 
 exit 0
